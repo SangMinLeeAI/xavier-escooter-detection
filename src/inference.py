b1372e6ae70514a130c8429535ee91d72ce34245
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2023-11-30 01:10:40 +0900

    fix: change input dimension and detection results

--- src/inference.py
+++ src/inference.py
@@ -1,89 +1,79 @@
 from typing import List, Dict
 
+import numpy as np
 import torch
 from PIL import Image
-from numpy import ndarray
 from torch import Tensor
 from torchvision.models import MobileNetV3
 from torchvision.models.detection import FasterRCNN
 from torchvision.transforms import functional as F
-
-import pretrained
-
-
-def resize_box(box: ndarray, factor: float = 0.3) -> ndarray:
-    """Resize the box coordinates."""
-    box[0] -= int((box[2] - box[0]) * factor)
-    box[1] -= int((box[3] - box[1]) * factor)
-    box[2] += int((box[2] - box[0]) * factor)
-    box[3] += int((box[3] - box[1]) * factor)
-    return box
+from torchvision import transforms
 
 
-def crop_and_transform(image: Image, box: ndarray, device: str) -> Tensor:
-    """Crop the image using the given box coordinates and transform it to a tensor."""
-    box_image = image.crop(box)
-    box_image_tensor = F.toTensor()(box_image).to(device)
-    return box_image_tensor
-
-
-def is_person_detected(result: List[Dict[Tensor]]) -> bool:
-    """Check if a person is detected."""
-    boxes = result[0]["boxes"].cpu().numpy().astype(int)
-    labels = result[0]["labels"].cpu().numpy()
-    scores = result[0]["scores"].cpu().numpy()
-
-    for box, label, score in zip(boxes, labels, scores):
-        if score > 0.5 and label == 1:
-            return True
-    return False
-
-
-def is_scooter_detected(model: MobileNetV3, image_tensor: Tensor) -> bool:
-    """Check if a scooter is detected."""
-    with torch.no_grad():
-        result = model(image_tensor)
-        predicted = (torch.sigmoid(result) > 0.5).float()
-        return predicted == 1
-
-
-def is_helmet_detected(model: FasterRCNN, image_tensor: Tensor) -> bool:
-    """Check if a helmet is detected."""
-    with torch.no_grad():
-        result = model(image_tensor)
-        helmet_labels = result[0]["labels"].cpu().numpy()
-        return 1 in helmet_labels or 2 in helmet_labels or 3 in helmet_labels
+def change_image_for_scooter(image: Tensor) -> Tensor:
+    image: Tensor = F.resize(image, (224, 224))
+    image: Tensor = image.unsqueeze(0)
+    return image
 
 
 def inference(
     helmet_model: FasterRCNN,
     person_model: FasterRCNN,
     scooter_model: MobileNetV3,
-    image_path: str,
+    image: Image,
     device: str = "cpu",
-) -> int:
-    # TODO input 이미지에 대한 inference 결과를 그려 반환하도록 구현
-    # TODO input을 동영상으로 받도록 구현
-
-    """Perform inference on the given image."""
+):
+    # 초기 세팅
     helmet_model.eval()
     person_model.eval()
     scooter_model.eval()
 
-    image = Image.open(image_path)
-    image_tensor = F.toTensor()(image).to(device)
+    helmet_model.to(device)
+    person_model.to(device)
+    scooter_model.to(device)
 
+    # 먼저 사람인지 인식하고 사람이면 스쿠터를 탔는지 확인 후 헬멧을 썼는지 확인
+
+    # 이미지 불러오기
+
+    image_tensor: Tensor = transforms.ToTensor()(image).unsqueeze(0).to(device)
+
+    # 사람인지 확인
+    person_result: List[Dict[str, Tensor]] = person_model(image_tensor)
+    boxes: np.ndarray = person_result[0]["boxes"].data.cpu().numpy()
+    labels: np.ndarray = person_result[0]["labels"].data.cpu().numpy()
+    scores: np.ndarray = person_result[0]["scores"].data.cpu().numpy()
+    to_delete: List = []
+    person_with_helmet: Dict[int, bool] = {}
     with torch.no_grad():
-        person_inference_result = person_model(image_tensor)
-    if is_person_detected(person_inference_result):
-        boxes = person_inference_result[0]["boxes"].cpu().numpy().astype(int)
-
-        for box in boxes:
-            resized_box = resize_box(box)
-            box_image_tensor = crop_and_transform(image, resized_box, device)
-
-            if is_scooter_detected(scooter_model, box_image_tensor):
-                if is_helmet_detected(helmet_model, box_image_tensor):
-                    return 1
-
-    return 0
+        for index, (box, label, score) in enumerate(zip(boxes, labels, scores)):
+            if label > 1 or score < 0.5:  # 사람이 아니면
+                to_delete.append(index)
+            else:
+                ## box 좌표를 이용해서 box 범위를 30% 넓힌다
+                box[0] = box[0] - int((box[2] - box[0]) * 0.7)
+                box[1] = box[1] - int((box[3] - box[1]) * 0.3)
+                box[2] = box[2] + int((box[2] - box[0]) * 0.7)
+                box[3] = box[3] + int((box[3] - box[1]) * 0.3)
+                # 박스를 이미지로 자른다
+                box_image: Image = image.crop(box)
+                box_image_tensor: Tensor = transforms.ToTensor()(box_image).to(device)
+                # 스쿠터 탔는지 확인
+                scooter_result: Tensor = scooter_model(
+                    change_image_for_scooter(box_image_tensor)
+                )
+                predicted: float = (torch.sigmoid(scooter_result) > 0.2).float().item()
+                if predicted == 1:
+                    # 헬멧 썼는지 확인
+                    helmet_result: List[Dict[str, Tensor]] = helmet_model(
+                        box_image_tensor.unsqueeze(0)
+                    )
+                    helmet_labels: List = helmet_result[0]["labels"].tolist()
+                    if 1 in helmet_labels:
+                        labels[index] = 2
+                    else:
+                        labels[index] = 3
+    box_result: List[int] = [i for j, i in enumerate(boxes) if j not in to_delete]
+    label_result: List[int] = [i for j, i in enumerate(labels) if j not in to_delete]
+    score_result: List[int] = [i for j, i in enumerate(scores) if j not in to_delete]
+    return box_result, label_result, score_result
